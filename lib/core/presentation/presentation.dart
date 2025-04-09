8378f454463392840ea1bcabe9becf625d99f215
Rick Krystianne Lim <rickimaru18@gmail.com>
2025-04-09 17:11:22 +0800

    feat(core): added form field widgets

--- lib/core/presentation/presentation.dart
+++ lib/core/presentation/presentation.dart
@@ -1,4 +1,6 @@
 export 'app_cubit.dart';
 export 'widgets/errors/try_again_empty.dart';
 export 'widgets/errors/try_again_error.dart';
+export 'widgets/form_fields/app_dropdown_field.dart';
+export 'widgets/form_fields/app_text_field.dart';
 export 'widgets/loaders/center_loader.dart';
