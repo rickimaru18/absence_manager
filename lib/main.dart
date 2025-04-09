98161f28bf2458e3f7e0a90767c95f8a578715e5
Rick Krystianne Lim <rickimaru18@gmail.com>
2025-04-09 09:44:50 +0800

    feat: setup DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'data/data.dart';
+import 'domain/domain.dart';
 import 'presentation/app.dart';
 
 void main() {
+  _setupDi();
   runApp(const App());
+}
+
+void _setupDi() {
+  setupDataDi();
+  setupDomainDi();
 }
