3a9f27efcec3110c6d1ee0dfba3111e9ce001b43
Rick Krystianne Lim <rickimaru18@gmail.com>
2025-04-09 19:28:14 +0800

    feat: support pagination in absences use case and repository

--- lib/domain/use_cases/absences_use_case.dart
+++ lib/domain/use_cases/absences_use_case.dart
@@ -9,17 +9,25 @@ class AbsencesUseCase {
 
   Future<Either<List<Absence>>> getAllAbsences({
     AbsenceFilter? filter,
+    required int offset,
+    int? limit,
   }) =>
       absencesRepository.getAbsences(
         filter: filter,
+        offset: offset,
+        limit: limit,
       );
 
-  Future<Either<List<Absence>>> getAbsencesByMember(
-    int userId, {
+  Future<Either<List<Absence>>> getAbsencesByMember({
     AbsenceFilter? filter,
+    required int userId,
+    required int offset,
+    int? limit,
   }) =>
       absencesRepository.getAbsences(
         filter: filter,
         userId: userId,
+        offset: offset,
+        limit: limit,
       );
 }
