af0eb743c780d581dabd20797b9d84ae4c7dc23f
Rick Krystianne Lim <rickimaru18@gmail.com>
2025-04-09 16:53:59 +0800

    feat(core): add "yyyy-MM-dd" date formatter

--- lib/core/utils/extensions/date_time_ext.dart
+++ lib/core/utils/extensions/date_time_ext.dart
@@ -1,5 +1,12 @@
 import 'package:intl/intl.dart';
 
 extension DateTimeExt on DateTime {
+  /// Format date to "Wed, 1/1/2025".
   String yMEd() => DateFormat.yMEd().format(this);
+
+  /// Format date to "1/1/2025".
+  String yMd() => DateFormat.yMd().format(this);
+
+  /// Format date to "2025-01-01".
+  String yyyyMMdd() => DateFormat('yyyy-MM-dd').format(this);
 }
