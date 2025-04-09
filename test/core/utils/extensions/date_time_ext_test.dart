90b70e6c3ff47395adf769434105be02ec7ab797
Rick Krystianne Lim <rickimaru18@gmail.com>
2025-04-09 16:54:26 +0800

    test(core): add tests for newly added date formatters

--- test/core/utils/extensions/date_time_ext_test.dart
+++ test/core/utils/extensions/date_time_ext_test.dart
@@ -12,4 +12,29 @@ void main() {
       'Tue, 12/31/2024',
     );
   });
+
+  test('[yMd] checks', () {
+    final DateTime firstDayOfYear = DateTime(2025);
+
+    expect(firstDayOfYear.yMd(), '1/1/2025');
+    expect(firstDayOfYear.add(const Duration(days: 1)).yMd(), '1/2/2025');
+    expect(
+      firstDayOfYear.subtract(const Duration(days: 1)).yMd(),
+      '12/31/2024',
+    );
+  });
+
+  test('[yyyyMMdd] checks', () {
+    final DateTime firstDayOfYear = DateTime(2025);
+
+    expect(firstDayOfYear.yyyyMMdd(), '2025-01-01');
+    expect(
+      firstDayOfYear.add(const Duration(days: 1)).yyyyMMdd(),
+      '2025-01-02',
+    );
+    expect(
+      firstDayOfYear.subtract(const Duration(days: 1)).yyyyMMdd(),
+      '2024-12-31',
+    );
+  });
 }
